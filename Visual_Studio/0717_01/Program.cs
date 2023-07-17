c6258d2ba998052a299fe4a4f029ad4397853ecf
SangjunHan <abcveve@gmail.com>
2023-07-17 17:12:48 +0900

    백업_강사님예제_추가

---
+++ Visual_Studio/0717_01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        static void A()
+        {
+            Console.WriteLine("A");
+        }
+
+        //functional overloading
+        //naming management
+        static int Add(int x, int y)
+        {
+            A();
+            return x + y;
+        }
+
+        static float Add(float x, float y)
+        {
+            return x + y;
+                }
+
+        static string Add(string x, string y) //반환값과 인자만 다르다
+        {
+            return x + y;
+        }
+
+/*        static T Add<T>(T x, T y) //제네틱메소드
+        {
+            return (T)(x) + (T)(y);
+     */   }
+        static void Main(string[] args)
+        {
+            Add(1,2);
+           Add(1.1f,2.2f); //int함수만 있었음 충돌난다
+            Add("Hello", "World"); //컴퓨터는 같은 add함수에 임의로 번호를 맥이고 처리함
+        }
+    }
+}
---
+++ Visual_Studio/0717_02/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0717_02
+{
+    internal class Program
+    {
+        static void swap(ref int a, ref int b)  //ref를 써야 참조하여 아래값을 가져다 쓴다
+        {
+            int c = 0;
+            c = b;
+            b = a;
+            a = c;
+
+            /* Console.WriteLine(a);
+             Console.WriteLine(b);*/
+        }
+
+        //sum, avg;
+
+        static void Calculate(int a, int b, out int outSum, out float outAvg) //out은 값만 받아온다, 아예 outsum, outavg라고 한다
+        {
+            outSum = a + b;
+            outAvg = (float)outSum / 2.0f;
+        }
+
+
+        //위와 아래는 별개로 계산하기에 위 작업후 아래로 갈때 전에했던 정보를 날린다
+        //위의 a와 아래 a는 다르다. 다른함수의 적용을 하지않는다
+
+        static void Main(string[] arg)
+        {
+            int a = 1;
+            int b = 2;
+
+            swap(ref a, ref b);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            int sum = 0;
+            float avg = 0;
+            Calculate(a, b, out sum, out avg);
+        }
+
+        /*static void Main(string[] args)
+        {
+            int a = 100;
+            {
+                int a = 10;
+                Console.WriteLine(a)
+            }
+            Console.WriteLine(a) //둘다 10나온다.c#은 막지만 c언어는 컴파일가능
+                        }
+*/
+    }
+}
