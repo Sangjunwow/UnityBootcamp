05315ee71642dc7fa77ebc257c34056f8e865322
SangjunHan <abcveve@gmail.com>
2023-08-07 17:47:40 +0900

    0807 복습

---
+++ Visual_Studio/C#이 보이는 그림책/065_checked, unchecked 문/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _065_checked__unchecked_문
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            try
+            {
+                checked//기본값에는 unchecked상태로 설정됨
+                {
+                    a = int.MaxValue; //int.MaxValue는 int의 최댓값
+                    b = a + 1; // 1을 더해 오버플로 발생
+                    Console.WriteLine(b);
+                }
+            }
+            catch (OverflowException oe)//checked를 지정한 경우 오버플로 한 경우에 수행하는 예외처리도 작성해야함. unchecked는 해당사항없음
+            {
+                Console.WriteLine(oe.Message);
+            }
+        }
+    }
+}
---
+++ Visual_Studio/C#이 보이는 그림책/71_if문(1)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _71_if문_1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 5;
+            if (a % 2 == 0)
+            {
+                Console.WriteLine(a + "는 짝수입니다.");
+            }
+            else
+            {
+                Console.WriteLine(a + "는 홀수입니다.");
+            }
+            int s = 65;
+            Console.WriteLine("당신의 점수는" + s + "점입니다.");
+            if (s < 70)
+            {
+                // {}을 하나로 묶는것을 블록이라고 한다
+                Console.WriteLine("평균까지 앞으로" + (70-s) + "점입니다.");
+                Console.WriteLine("분발하세요!");
+            }
+            else
+            {
+                Console.WriteLine("잘 했습니다!");
+            }
+        }
+    }
+}
